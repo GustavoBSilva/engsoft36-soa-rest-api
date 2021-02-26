bde21e7036b7063684e06adc2cc1575c494d6058
Gustavo Bianco Silva <gustavobianco@tic.ufrj.br>
2021-02-26 11:31:36 -0300

    Incluindo arquivo de configuração do BD e refatorando app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+from db import config_db
 
+app = Flask(__name__)
 api = Api(app)
 
 class MyApi(Resource):
     def get(self):
-        return { 'message': 'My First Flask RESTful API!'}
+        return {'message': 'My First Flask RESTful API!'}
 
 api.add_resource(MyApi, '/')
 
+def initialize_app(app):
+    config_db(app)
+
+
 if __name__ == '__main':
+    initialize_app(app)
     app.run(debug=True)
